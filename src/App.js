f537c622af3ef48f94b225393b047b4680c9b172
kasiaknapowska <k.knapowska@gmail.com>
2023-02-28 17:54:42 +0100

    brand color theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import logo from "./logo.svg";
 import logo from "./images/BeskidzkieLogo.png";
 import "./App.css";
+import { ThemeProvider } from '@mui/material/styles';
 import { Container } from "@mui/material";
 import Form from "./components/Form/Form";
+import {theme} from "./theme/theme"
 
 function App() {
   return (
     <div className="App">
-      <Container
+     <ThemeProvider theme={theme}>
+     <Container
         maxWidth="sm"
         sx={{
           paddingBottom: "200px",
@@ -20,6 +23,7 @@ function App() {
         <img src={logo} className="logo" alt="logo" />
         <Form />
       </Container>
+     </ThemeProvider>
     </div>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2d3d8a',
+        },
+        secondary: {
+            main: '#ffb922',
+        },
+    },
+    status: {
+      danger: '#d32f2f',
+    },
+  });
